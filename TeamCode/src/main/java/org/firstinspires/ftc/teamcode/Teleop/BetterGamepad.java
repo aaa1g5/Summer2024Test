56d6bb94d97cdab5ad7ae593fb3a64a0c200c5ab
aaa1g5 <rylhwry480@gmail.com>
2024-08-15 18:39:35 +0300

    ver0.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/BetterGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/BetterGamepad.java
@@ -1,2 +1,70 @@
-package org.firstinspires.ftc.teamcode.Teleop;public class BetterGamepad {
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class BetterGamepad {
+    private final Gamepad gamepad;
+
+    private int dpad_up, dpad_down, dpad_left, dpad_right;
+    private int tri, cir, x, squ;
+    private int left_bumper, right_bumper;
+    private int share, touchpad;
+    private int left_stick_button;
+
+    public double left_stick_x, right_stick_x, left_stick_y, right_stick_y;
+    public double left_trigger, right_trigger;
+
+    public BetterGamepad(Gamepad g) {
+        gamepad = g;
+    }
+
+    public void update() {
+        if (gamepad.triangle) { ++tri; } else { tri = 0; }
+        if (gamepad.circle) { ++cir; } else { cir = 0; }
+        if (gamepad.x) { ++x; } else { x = 0; }
+        if (gamepad.square) { ++squ; } else { squ = 0; }
+        if (gamepad.dpad_up) { ++dpad_up; } else { dpad_up = 0; }
+        if (gamepad.dpad_down) { ++dpad_down; } else { dpad_down = 0; }
+        if (gamepad.dpad_left) { ++dpad_left; } else { dpad_left = 0; }
+        if (gamepad.dpad_right) { ++dpad_right; } else { dpad_right = 0; }
+        if (gamepad.left_bumper) { ++left_bumper; } else { left_bumper = 0; }
+        if (gamepad.right_bumper) { ++right_bumper; } else { right_bumper = 0; }
+        if (gamepad.share) { ++share; } else { share = 0; }
+        if (gamepad.touchpad) { ++touchpad; } else { touchpad = 0; }
+        if (gamepad.left_stick_button) {++left_stick_button; } else {left_stick_button =0; }
+
+        left_stick_x = gamepad.left_stick_x;
+        left_stick_y = gamepad.left_stick_y;
+        right_stick_x = gamepad.right_stick_x;
+        right_stick_y = gamepad.right_stick_y;
+        left_trigger = gamepad.left_trigger;
+        right_trigger = gamepad.right_trigger;
+    }
+
+    public boolean dpadUp() { return 0 < dpad_up; }
+    public boolean dpadDown() { return 0 < dpad_down; }
+    public boolean dpadLeft() { return 0 < dpad_left; }
+    public boolean dpadRight() { return 0 < dpad_right; }
+    public boolean Tri() { return 0 < tri; }
+    public boolean Cir() { return 0 < cir; }
+    public boolean X() { return 0 < x; }
+    public boolean Squ() { return 0 < squ; }
+    public boolean leftBumper() { return 0 < left_bumper; }
+    public boolean rightBumper() { return 0 < right_bumper; }
+
+    public boolean shareOnce() { return 1 == share; }
+    public boolean touchpadOnce() { return 1 == touchpad; }
+    public boolean dpadUpOnce() { return 1 == dpad_up; }
+    public boolean dpadDownOnce() { return 1 == dpad_down; }
+    public boolean dpadLeftOnce() { return 1 == dpad_left; }
+    public boolean dpadRightOnce() { return 1 == dpad_right; }
+    public boolean TriOnce() { return 1 == tri; }
+    public boolean CirOnce() { return 1 == cir; }
+    public boolean XOnce() { return 1 == x; }
+    public boolean SquOnce() { return 1 == squ; }
+    public boolean leftBumperOnce() { return 1 == left_bumper; }
+    public boolean rightBumperOnce() { return 1 == right_bumper; }
+
+    public boolean leftStickButton() { return 1 == left_stick_button; }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Wheels.java
@@ -12,21 +12,23 @@ import com.qualcomm.robotcore.util.Range;
 @Config
 public class Wheels extends OpMode
 {
+    BetterGamepad betterGamepad = null;
 
     DcMotor motorLeftFront = null;
     DcMotor motorLeftBack = null;
     DcMotor motorRightFront = null;
     DcMotor motorRightBack = null;
 
-    double lY = 0;
-    double rY = 0;
-    double lX = 0;
-    double rX = 0;
-    public static double wheelsPower = 0.25;
+    double speed = 0;
+    double turn = 0;
+    double rotation = 0;
+    public static double wheelsPower = 1;
 
 
     @Override
     public void init(){
+        betterGamepad = new BetterGamepad(gamepad1);
+
         motorLeftFront = hardwareMap.get(DcMotor.class, "mFL");
         motorLeftBack = hardwareMap.get(DcMotor.class, "mBL");
         motorRightFront = hardwareMap.get(DcMotor.class, "mFR");
@@ -46,33 +48,38 @@ public class Wheels extends OpMode
 
     @Override
     public void start(){
-        telemetry.addLine("WE ARE VENOM!");
+        telemetry.addLine("blip, blop. initiated");
         telemetry.update();
-
     }
 
     @Override
     public void loop(){
-        telemetry.addLine("WE ARE VENOM!!");
-        telemetry.update();
+        speed = -gamepad1.left_stick_y;
+        turn = -gamepad1.right_stick_x;
+        rotation = gamepad1.left_stick_x;
 
-        if (gamepad1.square){
-            telemetry.addLine("pressed square");
-            wheelsPower = 0.5;
+        if (gamepad1.left_stick_button)
+        {
+            if (wheelsPower == 1)
+                wheelsPower = 0.5;
+            else
+                wheelsPower = 1;
+            telemetry.addLine("pressedBbum");
         }
-        if (gamepad1.circle){
-            telemetry.addLine("pressed square");
-            wheelsPower = 0.25;
+
+        if (betterGamepad.CirOnce())
+        {
+            telemetry.addLine("pressedBcir");
         }
 
-        motorLeftFront.setPower(wheelsPower);
-        motorRightFront.setPower(wheelsPower);
-        motorLeftBack.setPower(wheelsPower);
-        motorRightBack.setPower(wheelsPower);
-        wheelsPower = 0;
+        motorLeftFront.setPower( (speed+turn+rotation) * wheelsPower );
+        motorRightFront.setPower( (speed-turn-rotation) * wheelsPower );
+        motorLeftBack.setPower( (speed+turn-rotation) * wheelsPower );
+        motorRightBack.setPower( (speed-turn+rotation) * wheelsPower );
 
         telemetry.update();
-
+        betterGamepad.update();
+        telemetry.clear();
     }
 
     @Override
